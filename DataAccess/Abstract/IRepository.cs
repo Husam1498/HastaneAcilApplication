4a11a50156c0641ac56e30ccb96f3c6d50fbad2c
Hüsamettin demir <husamettin1498@gmail.com>
2024-08-23 01:21:39 +0300

    Data acces Katmanına sunlar eklendi: -entity,WebUikatmanı referans olark eklendi -entity-framework paketleri eklendi -abstract ve concrete klasorlari oluşturldu BUnlar abstact içinde interfaceler var concrete de bunları kalıtım yholuyla metodlarını alır -abstact ve concrete klasorlarine tablolar ile ilgili crud işlemlerini yapan kodlar yazıldı

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+
+        TEntity GetById(int id);
+
+        List<TEntity> GetAll();
+
+        void Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ DataAccess/Concrete/EfCore/MsSqlContext.cs
@@ -0,0 +1,26 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EfCore
+{
+    public class MsSqlContext:DbContext
+    {
+        public DbSet<Doktor> DoktorLar { get; set; }
+        public DbSet<AcilServisAlan> Alanlar { get; set; }
+        public DbSet<Hasta> Hastalar { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-S32BE3K;Database=HastaneAcilServisDb; Trusted_Connection=True; TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
