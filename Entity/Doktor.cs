16fe357f661c696633ed6ee1910e193e79a27745
Hüsamettin demir <husamettin1498@gmail.com>
2024-08-23 00:32:32 +0300

    EntityKatmanı Eklendi

---
+++ Entity/Doktor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Doktor
+    {
+        public int DoktorId { get; set; }
+        public string DoktorFUllname { get; set; }
+        public List<Hasta> Hastalar { get; set; }
+        public AcilServisAlan Alan { get; set; }
+        public int AlanId { get; set; }
+    }
+}
---
+++ Entity/Hasta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Hasta
+    {
+        public int HastaId { get; set; }
+        public string HastaFullname { get; set; }
+        public string DateBirth { get; set; }
+        public string HastlıkBilgisi { get; set; }
+
+        public Doktor doktor { get; set; }
+        public int DoktorId { get; set; }
+    }
+}
