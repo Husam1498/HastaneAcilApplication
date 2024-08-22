74f27a46f91fe6ce980c77f93ca6ed88da7bcc63
Hüsamettin demir <husamettin1498@gmail.com>
2024-08-22 23:01:46 +0300

    Add project files.

---
+++ AplicationWebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicationWebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
