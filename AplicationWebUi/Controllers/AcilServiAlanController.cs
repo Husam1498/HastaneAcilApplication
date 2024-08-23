93b7797d47315f52273c84c94e17ffd3b79232e8
Hüsamettin demir <husamettin1498@gmail.com>
2024-08-24 01:30:54 +0300

    Hastaları gösterme ve ekleme  classları eklendi

---
+++ AplicationWebUi/Controllers/AcilServiAlanController.cs
@@ -0,0 +1,31 @@
+﻿using Bussiness.Abstract;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicationWebUi.Controllers
+{
+    public class AcilServiAlanController : Controller
+    {
+        private IAcilServisAlanService _service;
+
+        public AcilServiAlanController(IAcilServisAlanService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(AcilServisAlan entity)
+        {
+            _service.Create(entity);
+            return RedirectToAction(nameof(List));
+        }
+        public IActionResult List()
+        {
+            return View(_service.GetAll());
+        }
+    }
+}
--- AplicationWebUi/Program.cs
+++ AplicationWebUi/Program.cs
@@ -1,4 +1,8 @@
 using AplicationWebUi.identity;
+using Bussiness.Abstract;
+using Bussiness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EfCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +16,37 @@ builder.Services.AddDbContext<MsSQLContext>(options =>
 );
 builder.Services.AddIdentity<User,IdentityRole>().AddEntityFrameworkStores<MsSQLContext>().AddDefaultTokenProviders();//kullanýcý login olduðunda cookide benzersiz bir token oluþturur
 
+builder.Services.Configure<IdentityOptions>(opt =>
+{
+    opt.Password.RequireDigit = true;
 
+    opt.Lockout.MaxFailedAccessAttempts = 5;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(5);
+    opt.Lockout.AllowedForNewUsers = true;
+
+    opt.User.RequireUniqueEmail = true;
+    //opt.SignIn.RequireConfirmedEmail = true;//Kullanýcý kayýt olurken emailini doðrulamasý gerekr
+});
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.LoginPath="/Kullanici/Login";
+    opt.LogoutPath = "/Kullanici/Logout";
+    opt.AccessDeniedPath = "/Kullanici/AccessDenied";
+
+    opt.SlidingExpiration = true;
+    opt.ExpireTimeSpan = TimeSpan.FromDays(5);
+
+    opt.Cookie = new CookieBuilder
+    {
+        HttpOnly = true,//javascript uygulamlarý cokiye ulaþmaamýs için
+        Name=".HastaneCookies"
+    };
+
+});
+
+builder.Services.AddScoped<IAcilServisAlanRepository, EfCoreAcilAlanRepository>();
+
+builder.Services.AddScoped<IAcilServisAlanService, AcilServisAlanManager>();
 
 
 var app = builder.Build();
