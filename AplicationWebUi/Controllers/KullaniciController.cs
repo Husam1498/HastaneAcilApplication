1a7a720e42debe6b8114535179944c767a28d0be
Hüsamettin demir <husamettin1498@gmail.com>
2024-09-14 16:10:08 +0300

    Css <h1> etiketlerine animasyonlar eklendi

--- AplicationWebUi/Controllers/KullaniciController.cs
+++ AplicationWebUi/Controllers/KullaniciController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
 
 namespace AplicationWebUi.Controllers
